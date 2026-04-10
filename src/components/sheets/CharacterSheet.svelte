58b748946ae6005a05b5279fa11aba346ba63590
mountogdengc <mountogdengc@users.noreply.github.com>
2026-04-10 17:54:43 -0600

    feat: add proof-of-life Svelte 5 character sheet component

---
+++ src/components/sheets/CharacterSheet.svelte
@@ -0,0 +1,18 @@
+<script>
+  let { document: actorDocument } = $props();
+
+  let actor = $state(actorDocument);
+
+  $effect(() => {
+    const hookId = Hooks.on("updateActor", (updatedActor) => {
+      if (updatedActor.id === actor.id) {
+        actor = updatedActor;
+      }
+    });
+    return () => Hooks.off("updateActor", hookId);
+  });
+</script>
+
+<div class="p-4 text-lg font-bold text-white bg-gray-800 rounded">
+  {actor.name}
+</div>
