8b1e71c755d28c2288139bfe00b96a73ea90de5a
mountogdengc <mountogdengc@users.noreply.github.com>
2026-04-12 22:06:19 -0600

    feat: add BESM status effect definitions

---
+++ src/combat/statusEffects.mjs
@@ -0,0 +1,37 @@
+export const BESM_STATUS_EFFECTS = [
+  {
+    id: "stunned",
+    name: "Stunned",
+    icon: "icons/svg/daze.svg",
+  },
+  {
+    id: "prone",
+    name: "Prone",
+    icon: "icons/svg/falling.svg",
+  },
+  {
+    id: "unconscious",
+    name: "Unconscious",
+    icon: "icons/svg/unconscious.svg",
+  },
+  {
+    id: "dead",
+    name: "Dead",
+    icon: "icons/svg/skull.svg",
+  },
+  {
+    id: "energyDepleted",
+    name: "Energy Depleted",
+    icon: "icons/svg/lightning.svg",
+  },
+  {
+    id: "burning",
+    name: "Burning",
+    icon: "icons/svg/fire.svg",
+  },
+  {
+    id: "bound",
+    name: "Bound",
+    icon: "icons/svg/net.svg",
+  },
+];
